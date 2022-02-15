ceff20e8293acd948348ecb5cf9d19ac885f5e8a
Fernando Mar <73912926+feralucard2@users.noreply.github.com>
2022-02-15 11:49:44 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'la4ld342022' );
+define( 'DB_NAME', 'laaldeadev' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'forge' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'ipJlBz5ays4ho80oULXY' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
